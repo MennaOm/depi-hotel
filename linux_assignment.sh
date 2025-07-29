1a25dac4438aa3dd6fbd17d3bd3d1a4e3a8ffffb
MennaOm <omarmenna041@gmail.com>
2025-07-29 14:01:27 +0300

    Add Linux assignment scri pt

---
+++ linux_assignment.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+sudo adduser rody
+sudo deluser rody
+touch assignment.txt
+chmod 752 assignment.txt
+chmod u+x assignment.txt
+mkdir ass_folder
+pwd
+ls
+rmdir ass_folder
+ls
+grep "write" assignment.txt
+cat assignment.txt
+history
+date
+pwd
+sudo addusr devops_menna
+su - devops_menna
+alias gs='git status'
+cp assignment.txt ass.txt
+mv ass.txt moved.txt
+mv test.txt tst.txt
+
+
+
